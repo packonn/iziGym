ffd7fa1f3a061170622b26514a40a51ed28302be
packonn <60152272+packonn@users.noreply.github.com>
2023-05-07 12:02:44 +0200

    navabar reste responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import { Fragment } from "react"
 import { Disclosure, Menu, Transition } from "@headlessui/react"
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline"
 import Image from "next/image"
+import Button from "./Button"
 
 const navigation = [
 	{ name: "Accueil", href: "#", current: true },
@@ -22,7 +23,7 @@ export default function Navbar() {
 				<>
 					<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 						<div className="relative flex h-16 items-center justify-between">
-							<div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+							<div className="absolute inset-y-0 right-0 flex items-center lg:hidden">
 								{/* Mobile menu button*/}
 								<Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
 									<span className="sr-only">
@@ -30,18 +31,18 @@ export default function Navbar() {
 									</span>
 									{open ? (
 										<XMarkIcon
-											className="block h-6 w-6"
+											className="block h-6 w-6 text-white"
 											aria-hidden="true"
 										/>
 									) : (
 										<Bars3Icon
-											className="block h-6 w-6"
+											className="block h-6 w-6 text-white"
 											aria-hidden="true"
 										/>
 									)}
 								</Disclosure.Button>
 							</div>
-							<div className="flex items-center w-full  sm:justify-around ">
+							<div className="flex items-center w-full  lg:justify-around ">
 								<div className="flex flex-shrink-0 items-center">
 									<img
 										className="block h-8 w-auto lg:hidden"
@@ -54,7 +55,7 @@ export default function Navbar() {
 										alt="Your Company"
 									/>
 								</div>
-								<div className="hidden sm:ml-6 sm:block">
+								<div className="hidden sm:ml-6 lg:block">
 									<div className="flex space-x-4">
 										{navigation.map((item) => (
 											<a
@@ -64,7 +65,7 @@ export default function Navbar() {
 													item.current
 														? " text-white border-b-2 border-primary"
 														: "text-white  hover:text-primary",
-													" px-3 py-2 text-sm font-medium"
+													" px-2 py-2 text-sm uppercase tracking-widest"
 												)}
 												aria-current={
 													item.current
@@ -76,17 +77,12 @@ export default function Navbar() {
 											</a>
 										))}
 										<div className="absolute inset-y-0 right-0 flex justify-around items-center sm:static sm:inset-auto sm:ml-6 sm:pr-0 bg-primary w-40 h-8">
-											<Image
-												src={"/logo/fleche_blanche.png"}
-												width={16}
-												height={16}
-											/>
-											<button
+											<Button
 												type="button"
-												className="text-white text-base"
-											>
-												Inscription
-											</button>
+												className="text-white text-base "
+												text={"inscription"}
+												icon={"/assets-dev/arrow.svg"}
+											></Button>
 										</div>
 									</div>
 								</div>
@@ -94,7 +90,7 @@ export default function Navbar() {
 						</div>
 					</div>
 
-					<Disclosure.Panel className="sm:hidden">
+					<Disclosure.Panel className="lg:hidden h-full bg-white">
 						<div className="space-y-1 px-2 pb-3 pt-2">
 							{navigation.map((item) => (
 								<Disclosure.Button
@@ -102,10 +98,7 @@ export default function Navbar() {
 									as="a"
 									href={item.href}
 									className={classNames(
-										item.current
-											? "bg-gray-900 text-white"
-											: "text-gray-300 hover:bg-gray-700 hover:text-white",
-										"block rounded-md px-3 py-2 text-base font-medium"
+										"block rounded-md px-3 py-2 text-base font-medium text-white  hover:text-primary"
 									)}
 									aria-current={
 										item.current ? "page" : undefined
