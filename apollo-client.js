a2929ffa1ae43fb823c0854cb0711a88a01b6a19
Amande <69899871+amandine16@users.noreply.github.com>
2023-06-03 15:35:09 +0200

    add apollo client

---
+++ apollo-client.js
@@ -0,0 +1,10 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+
+
+const apolloClient = new ApolloClient({
+    uri: process.env.BACKEND_URL + "xgraphql",
+    cache: new InMemoryCache(),
+});
+
+export default apolloClient;
