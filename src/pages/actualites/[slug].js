a64488214d0343ee571a86ed1a67618cc21597ed
packonn <60152272+packonn@users.noreply.github.com>
2023-06-05 22:17:08 +0200

    actu avc vrai donnée

--- src/pages/actualites/[slug].js
+++ src/pages/actualites/[slug].js
@@ -1,7 +1,6 @@
 import { NextPage } from "next"
 import { useRouter } from "next/router"
 import Layout from "@/components/Layout"
-
 import Image from "next/image"
 import { useEffect, useState } from "react"
 import VideoPlayer from "@/components/Video"
@@ -13,25 +12,48 @@ import {
 	images,
 	slidesPerView,
 } from "../../../helpers"
+import apolloClient from "../../../apollo-client"
+import { gql } from "@apollo/client"
 
 export const getServerSideProps = async (context) => {
-	const slug = context.params
+	const slug = context.params.slug
 	console.log(slug)
 
 	let actu = {}
 
-	// try {
-	// 	const response = await apolloClient.query({
-	// 		query: gql`
-	// 			{
-
-	// 			}
-	// 		`,
-	// 	})
-	// 	actus = await response
-	// } catch (error) {
-	// 	console.log("error", error)
-	// }
+	try {
+		const response = await apolloClient.query({
+			query: gql`
+			{
+				post(id: "${slug}", idType: SLUG) {
+				  title
+				  content
+				  featuredImage {
+					node {
+					  sourceUrl
+					}
+				  }
+				  slug
+				  groupeChampsArticle {
+					enddate
+					startdate
+					subtitle
+					videourl
+					gallery {
+					  sourceUrl
+					}
+					actuimage {
+					  sourceUrl
+					}
+				  }
+				}
+			  }
+			`,
+		})
+		actu = await response.data.post
+	} catch (error) {
+		console.log("error", error)
+	}
 
 	return {
 		props: {
@@ -40,58 +62,62 @@ export const getServerSideProps = async (context) => {
 	}
 }
 
-const ActualiteSlug = ({ slug }) => {
+const ActualiteSlug = ({ slug, actu }) => {
+	console.log(actu)
 	const router = useRouter()
 	const [showVideo, setShowVideo] = useState(false)
 
-	// useEffect(() => {
-	// 	setShowVideo(true)
-	// }, [])
+	useEffect(() => {
+		setShowVideo(true)
+	}, [])
 
 	return (
-		<div></div>
-		// <Layout
-		// 	backgroundImageURL="/assets-dev/header.png"
-		// 	title1="Club"
-		// 	title2="IZI GYM"
-		// 	title3="Deviens la meileure version de toi mêmessss !"
-		// 	center
-		// >
-		// 	<div className="container md:w-[884px] w-full pb-[80px] -mt-[120px]">
-		// 		<div className="  relative h-[600px] ">
-		// 			<Image
-		// 				fill
-		// 				className="w-full h-full right-10 object-cover "
-		// 				src="/assets-dev/header.png"
-		// 				alt="header"
-		// 			/>
-		// 		</div>
-		// 		<div className="   z-10 ">
-		// 			<div
-		// 				dangerouslySetInnerHTML={{
-		// 					__html: "<h1>titre</h1><p>jefzifheoz</p>",
-		// 				}}
-		// 				className="py-4"
-		// 			></div>
-		// 			{showVideo && (
-		// 				<div className="player-wrapper overflow-hidden p-[0px]  bg-black ">
-		// 					<VideoPlayer
-		// 						url="https://www.youtube.com/watch?v=NigrQ9UcJy4"
-		// 						isPlaying={false}
-		// 					/>
-		// 				</div>
-		// 			)}
-		// 			<div className="mt-4">
-		// 				<SwiperGallery
-		// 					data={[...images.map((item) => item)]}
-		// 					breakpoints={breakPointsSwiper}
-		// 					slidesPerView={slidesPerView}
-		// 				/>
-		// 			</div>
-		// 		</div>
-		// 	</div>
-		// 	<SectionActus data={actus.filter((e, i) => e.slug !== slug)} />
-		// </Layout>
+		<Layout
+			backgroundImageURL={actu.featuredImage.node.sourceUrl}
+			title1={actu.title}
+			title2={actu.groupeChampsArticle.subtitle}
+			title3={actu.groupeChampsArticle.startdate}
+			center
+		>
+			<div className="container md:w-[884px] w-full pb-[80px] -mt-[120px]">
+				<div className="  relative h-[600px] ">
+					<Image
+						fill
+						className="w-full h-full right-10 object-cover "
+						src={actu.groupeChampsArticle.actuimage.sourceUrl}
+						alt="header"
+					/>
+				</div>
+				<div className="   z-10 ">
+					<div
+						dangerouslySetInnerHTML={{
+							__html: `${actu.content}`,
+						}}
+						className="py-4"
+					></div>
+					{showVideo && (
+						<div className="player-wrapper overflow-hidden p-[0px]  bg-black ">
+							<VideoPlayer
+								url={actu.groupeChampsArticle.videourl}
+								isPlaying={false}
+							/>
+						</div>
+					)}
+					<div className="mt-4">
+						<SwiperGallery
+							data={[
+								...actu.groupeChampsArticle.gallery.map(
+									(item) => item.sourceUrl
+								),
+							]}
+							breakpoints={breakPointsSwiper}
+							slidesPerView={slidesPerView}
+						/>
+					</div>
+				</div>
+			</div>
+			<SectionActus data={actus.filter((e, i) => e.slug !== slug)} />
+		</Layout>
 	)
 }
 
