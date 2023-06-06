6776c1c391fba5776621444349edcadf20a80629
Amande <69899871+amandine16@users.noreply.github.com>
2023-06-06 21:13:20 +0200

    add loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,36 @@
 import "@/styles/globals.css"
-
+import { useEffect, useState } from 'react';
+import { Router } from 'next/router';
+import Loader from "@/components/Loader";
 
 export default function App({ Component, pageProps }) {
-	return (
-	
-			<Component {...pageProps} />
 
+	const [loading, setLoading] = useState(false);
+  
+  
+	useEffect(() => {
+	  Router.events.on('routeChangeStart', (url) => {
+		setLoading(true);
+  
+	  });
+  
+	  Router.events.on('routeChangeComplete', (url) => {
+		setTimeout(() => {
+		  setLoading(false);
+		}, 1000);
+	  });
+	}, []);
+  
+	return (
+	  <>
+		{loading && <Loader />}
+		<Component {...pageProps} />
+	  </>
+  
 	)
-}
+  }
+
+
+
+
+
