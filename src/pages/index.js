b6ea72011a14ba1d9581bab86392ac47fda2fff7
packonn <60152272+packonn@users.noreply.github.com>
2023-06-07 22:38:50 +0200

    test meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import { gql } from "@apollo/client"
 import apolloClient from "../../apollo-client"
 import dayjs from "dayjs"
 import "dayjs/locale/fr"
+import Head from "next/head"
 
 export const getServerSideProps = async (context) => {
 	let subscriptions = []
@@ -118,41 +119,49 @@ export const getServerSideProps = async (context) => {
 
 export default function Home({ subscriptions, options, spaces, actus }) {
 	return (
-		<Layout
-			contactBannerColor="cream"
-			backgroundImageURL="/assets-dev/header.png"
-			title1="Club"
-			title2="IZI GYM"
-			title3="La forme avant la frime !"
-			hours
-		>
-			<SectionServices spaces={spaces} />
-			<SectionActus bottomBanner actus={actus} />
-			<SectionWhy options={options} />
-			{/*<Planning
+		<>
+			<Head>
+				<meta
+					http-equiv="Content-Security-Policy"
+					content="upgrade-insecure-requests"
+				/>
+			</Head>
+			<Layout
+				contactBannerColor="cream"
+				backgroundImageURL="/assets-dev/header.png"
+				title1="Club"
+				title2="IZI GYM"
+				title3="La forme avant la frime !"
+				hours
+			>
+				<SectionServices spaces={spaces} />
+				<SectionActus bottomBanner actus={actus} />
+				<SectionWhy options={options} />
+				{/*<Planning
 				planning={options.planning}
 				urlDeReservatioDesCoursEnLigne={
 					options.urlDeReservatioDesCoursEnLigne
 				}
 			/>*/}
-			<div className=" bg-bottomHeader absolute h-40 w-full -mt-[150px] z-[9999]  bg-repeat"></div>
+				<div className=" bg-bottomHeader absolute h-40 w-full -mt-[150px] z-[9999]  bg-repeat"></div>
 
-			<iframe
-				loading="eager"
-				src={options.urlcalendar}
-				width="100%"
-				height="700px"
-				frameborder="0"
-				style={{ marginBottom: 10 + "em", marginTop: 5 + "em" }}
-			></iframe>
+				<iframe
+					loading="eager"
+					src={options.urlcalendar}
+					width="100%"
+					height="700px"
+					frameborder="0"
+					style={{ marginBottom: 10 + "em", marginTop: 5 + "em" }}
+				></iframe>
 
-			<div className="relative mt-20">
-				<div className="bg-[url(/assets-dev/wave-cream.svg)] h-40 w-full -top-40 absolute z-50  bg-no-repeat bg-cover "></div>
-				<SectionPrices
-					subscriptions={subscriptions}
-					infoSubscription={options.infosubscription}
-				/>
-			</div>
-		</Layout>
+				<div className="relative mt-20">
+					<div className="bg-[url(/assets-dev/wave-cream.svg)] h-40 w-full -top-40 absolute z-50  bg-no-repeat bg-cover "></div>
+					<SectionPrices
+						subscriptions={subscriptions}
+						infoSubscription={options.infosubscription}
+					/>
+				</div>
+			</Layout>
+		</>
 	)
 }
