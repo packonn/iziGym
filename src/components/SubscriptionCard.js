7a286efebea53d23d96c41fe89fc8c7fcbaa9327
Amande <69899871+amandine16@users.noreply.github.com>
2023-06-20 21:57:20 +0200

    fix bg-card price

--- src/components/SubscriptionCard.js
+++ src/components/SubscriptionCard.js
@@ -1,8 +1,9 @@
 export const SubscriptionCard = ({data,customContainerClass })=> {
+	const classes = customContainerClass || " odd:bg-white "
     return (
         <div
 							
-								className={` hover:scale-[1.02] transition-all  ease-in-out duration-500 even:bg-secondary even:text-white odd:bg-white  flex flex-col items-center lg:h-full  self-end min-h-[433px] lg:[&:nth-child(2)]:min-h-[488px]   [&:nth-child(4)]:self-start px-12 py-7 ${customContainerClass}`}
+								className={`${classes} hover:scale-[1.02] transition-all  ease-in-out duration-500 even:bg-secondary even:text-white  flex flex-col items-center lg:h-full  self-end min-h-[433px] lg:[&:nth-child(2)]:min-h-[488px]   [&:nth-child(4)]:self-start px-12 py-7 `}
 							>
 								<h3 className="text-[37px] font-anton uppercase text-center pb-5 ">
 									{data.title}
