192585d1dd7300df61152cd2c7b6c6e2348d7234
Amande <69899871+amandine16@users.noreply.github.com>
2024-07-28 14:08:11 +0200

    test

--- src/pages/redirect-whatsapp.js
+++ src/pages/redirect-whatsapp.js
@@ -5,21 +5,22 @@ import { useRouter } from "next/router"
 const RedirectPage = () => {
 	const router = useRouter()
 
-	useEffect(() => {
-		// URL de redirection
-		const redirectTo =
-			"https://wa.me/?text=Hey%20Viens%20me%20rejoindre%20chez%20IZI%20gym%20pour%20une%20sceance%20decouverte.%20Clique%20ici%20:%20https://reservation.izigym.fr"
-		// Effectuer la redirection
-		router.push(redirectTo)
-	}, [router])
+	// useEffect(() => {
+	// 	// URL de redirection
+	// 	const redirectTo =
+	// 		"https://wa.me/?text=Hey%20Viens%20me%20rejoindre%20chez%20IZI%20gym%20pour%20une%20sceance%20decouverte.%20Clique%20ici%20:%20https://reservation.izigym.fr"
+	// 	// Effectuer la redirection
+	// 	router.push(redirectTo)
+	// }, [router])
 
 	return (
 		<div>
 			<p>
-				Redirection en cours... Si vous n'êtes pas redirigé
-				automatiquement,{" "}
-				<a href="https://wa.me/?text=Hey%20Viens%20me%20rejoindre%20chez%20IZI%20gym%20pour%20une%20sceance%20decouverte.%20Clique%20ici%20:%20https://reservation.izigym.fr">
-					cliquez ici
+				<a
+					href="sms:?subject=I wanted you to see this site&amp;body=Check out this site http://www.website.com."
+					title="Share by Email"
+				>
+					££Partage££
 				</a>
 				.
 			</p>
