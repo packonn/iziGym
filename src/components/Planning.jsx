3433d87f33ecb10319f9e1485ad39218926a5185
Amande <69899871+amandine16@users.noreply.github.com>
2026-01-23 19:03:27 +0100

    update planning image + url booking

--- src/components/Planning.jsx
+++ src/components/Planning.jsx
@@ -49,8 +49,8 @@ const Planning = () => {
 							</div>
 						</div>
 					</div>
-					<Link href={"https://cms.izigym.fr/wp-content/uploads/2025/08/Copie-de-Planning-Septembre-2024-21-x-14.8-cm-Flyers-Paysage-2.png"} target="_blank">
-						<Image src="https://cms.izigym.fr/wp-content/uploads/2025/08/Copie-de-Planning-Septembre-2024-21-x-14.8-cm-Flyers-Paysage-2.png" alt="planning horaires cours collectif izi Gym" width={2000} height={1415} className="h-auto w-full mt-20 object-contain" />
+					<Link href={"https://cms.izigym.fr/wp-content/uploads/2026/01/Copie-de-Planning-Septembre-2024-21-x-14.8-cm-Flyers-Paysage-3.png"} target="_blank">
+						<Image src="https://cms.izigym.fr/wp-content/uploads/2026/01/Copie-de-Planning-Septembre-2024-21-x-14.8-cm-Flyers-Paysage-3.png" alt="planning horaires cours collectif izi Gym" width={2000} height={1415} className="h-auto w-full mt-20 object-contain" />
 					</Link>
 					{/* <div className="grid grid-cols-3 gap-10 mt-24 ">
 						{planning &&
